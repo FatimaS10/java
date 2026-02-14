0ccb614f5fde700ee1e5d7dbb75c14eaf0d8c159
FatimaS10 <shakifatimaislam@gmail.com>
2026-02-14 21:28:45 +0600

    Create Grade result Main.java
    grade (A+ to F)

---
+++ Grade result Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+ public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter your marks : ");
+        int marks = sc.nextInt();
+        if(marks>= 80 && marks<=100){
+            System.out.println("Grade : A+");
+        }
+        else if(marks>=70){
+            System.out.println("Grade : A" );
+            }
+            else if(marks>=60){
+                System.out.println("Grade : A-");
+            }else if(marks>=50){
+                System.out.println( "Grade : B");
+            }else if(marks>=40){
+                System.out.println( "Grade : C");
+            }else if(marks>=33){
+                System.out.println( "Grade : D");
+            }else if(marks>=0){
+                System.out.println( "Grade : F");
+            }else {
+                System.out.println( "Invalid Marks!");
+            }
+    }
+ }
